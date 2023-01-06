8a5aba5fc948982615c647c645289617b192de13
wingled22 <pelayowindel@gmail.com>
2023-01-06 21:02:21 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>AdminLTE 3 | Log in (v2)</title>
+
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="~/../plugins/fontawesome-free/css/all.min.css">
+        <!-- icheck bootstrap -->
+        <link rel="stylesheet" href="~/../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="~/../dist/css/adminlte.min.css">
+    </head>
+
+    <body class="login-page" style="min-height: 469.383px;">
+        <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+            <a href="../../index2.html" class="h1"><b>Admin Login</b></a>
+            </div>
+            <div class="card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+
+            <form action="../../index3.html" method="post">
+                <div class="input-group mb-3">
+                <input type="email" class="form-control" placeholder="Email">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                    <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+                </div>
+                <div class="input-group mb-3">
+                <input type="password" class="form-control" placeholder="Password">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col-8">
+                    
+                </div>
+                <!-- /.col -->
+                <div class="col-4">
+                    <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                </div>
+                <!-- /.col -->
+                </div>
+            </form>
+        </div>
+        <!-- /.card -->
+        </div>
+        <!-- /.login-box -->
+
+        <!-- jQuery -->
+        <script src="../../plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="../../dist/js/adminlte.min.js"></script>
+    </body>
+</html>
