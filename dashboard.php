00960599408944b77294478536fc2f362441dece
wingled22 <pelayowindel@gmail.com>
2023-01-09 17:19:33 +0800

    updated sidebar links

--- dashboard.php
+++ dashboard.php
@@ -49,46 +49,51 @@
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
-      </div>
+      </div> -->
 
      
 
+      
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Pages
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
+            <li class="nav-item">
+              <a href="employees.php" class="nav-link">
+                <i class="nav-icon fas fa-users"></i>
+                <p>
+                  Employees
+                </p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="applicants.php" class="nav-link">
+                <i class="nav-icon fas fa-user-plus"></i>
+                <p>
+                  Applicants
+                </p>
+              </a>
+            </li>
+          
+            <li class="nav-item">
+              <a href="wasa.php" class="nav-link">
+                <i class="nav-icon fas fa-money-bill"></i>
+                <p>
+                  WASA
+                </p>
+              </a>
+            </li>
+
+          <!-- <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
@@ -96,9 +101,10 @@
                 <span class="right badge badge-danger">New</span>
               </p>
             </a>
-          </li>
+          </li> -->
         </ul>
       </nav>
+      
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
@@ -166,7 +172,7 @@
               <div class="icon">
                 <i class="fa fa-user"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="wasa.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
          
--- php-includes/header.php
+++ php-includes/header.php
@@ -63,14 +63,14 @@
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
-      </div>
+      </div> -->
 
      
 
@@ -79,30 +79,34 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Pages
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
+            <li class="nav-item">
+              <a href="employees.php" class="nav-link">
+                <i class="nav-icon fas fa-users"></i>
+                <p>
+                  Employees
+                </p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="applicants.php" class="nav-link">
+                <i class="nav-icon fas fa-user-plus"></i>
+                <p>
+                  Applicants
+                </p>
+              </a>
+            </li>
+          
+            <li class="nav-item">
+              <a href="wasa.php" class="nav-link">
+                <i class="nav-icon fas fa-money-bill"></i>
+                <p>
+                  WASA
+                </p>
+              </a>
+            </li>
+
+          <!-- <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
@@ -110,7 +114,7 @@
                 <span class="right badge badge-danger">New</span>
               </p>
             </a>
-          </li>
+          </li> -->
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
